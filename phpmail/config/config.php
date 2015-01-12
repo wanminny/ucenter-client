847b363090fc5b568f5494078fbad92c96278eaa
wanmin <min.wan@yoho.cn>
2015-01-12 18:43:57 +0800

    password

--- phpmail/config/config.php
+++ phpmail/config/config.php
@@ -2,7 +2,7 @@
 //邮局用户名(请填写完整的email地址)
 define('MAILLOGINNAME','wanminny@163.com');//www@hiboxinc.com//
 //邮局密码
-define('MAILLOGINPASSWORD','nihaoma025');//Bhu8Mko0Nji9
+define('MAILLOGINPASSWORD','dontkonw');//Bhu8Mko0Nji9
 //企业邮局域名
 define('MAILHOST','smtp.163.com');//117.79.157.40
 ?>
